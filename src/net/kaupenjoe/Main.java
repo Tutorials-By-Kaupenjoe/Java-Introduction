e18b503fc5f5660c00b656899218911b48431485
Kaupenjoe <N.Kaupenjohann@gmail.com>
2021-10-29 16:01:20 +0200

    java4

--- src/net/kaupenjoe/Main.java
+++ src/net/kaupenjoe/Main.java
@@ -2,30 +2,13 @@ package net.kaupenjoe;
 
 public class Main {
     public static void main(String[] args) {
-        /* HOW TO READ CODE / Java Syntax */
+        /* Outputting Things with println */
 
-        // Code is read from top to bottom
+        System.out.println("Hello YouTube");
 
-        // This is a single line Comment
-
-        /*
-         *
-         * This is a
-         * Multiline Comment!
-         *
-         * */
-
-        // Declaring a Variable | Note that all lines end with a semi-colon!
-        int i;
-
-        // Assigning a Variable
-        i = 30;
-
-        // Declaring and Assigning (Initializing)
         int x = 10;
+        System.out.println(x);
 
-        // Access variables/method of Objects
-        String s = "Hello World";
-        s.length();
+        System.out.println("Outputting some Value: " + x);
     }
 }
