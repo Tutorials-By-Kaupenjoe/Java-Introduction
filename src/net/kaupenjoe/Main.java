bb8ff3bef2afeb68cd4035dda73017090971c559
Kaupenjoe <N.Kaupenjohann@gmail.com>
2021-11-02 18:14:22 +0100

    error examples

--- src/net/kaupenjoe/Main.java
+++ src/net/kaupenjoe/Main.java
@@ -2,13 +2,23 @@ package net.kaupenjoe;
 
 public class Main {
     public static void main(String[] args) {
-        /* Outputting Things with println */
+        /* Errors and how to get help with them */
 
-        System.out.println("Hello YouTube");
+        // Uncomment the lines to see examples of some of these errors!
 
-        int x = 10;
-        System.out.println(x);
+        // java: cannot find symbol
+        // "Something is red inside the Code"
+        // System.out.println(a231);
 
-        System.out.println("Outputting some Value: " + x);
+        // Exception in thread "main" java.lang.NullPointerException
+        // Something is null (doesn't have a value)
+        String s = null;
+        // System.out.println(s.length());
+
+
+        // Process finished with exit code 1
+        // "finished with non-zero exit value 1"
+        // NEVER only post this. This shows everyone you don't know programming or Java
+        // Usually there should be an error log/output or an "Exception"
     }
 }
