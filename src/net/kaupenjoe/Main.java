a4b3e68aad7b2ebfeff44f39015e59e3120c2194
Kaupenjoe <N.Kaupenjohann@gmail.com>
2021-11-08 07:59:00 +0100

    switch

--- src/net/kaupenjoe/Main.java
+++ src/net/kaupenjoe/Main.java
@@ -1,36 +1,16 @@
 package net.kaupenjoe;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        /* IF AND ELSE STATEMENTS  */
+        /* SWITCH STATEMENT  */
 
-        int exam1 = 65;
-        int exam2 = 51;
+        int medalPlacement = 1;
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Results for Exam 1: ");
-        exam1 = scanner.nextInt();
-        System.out.println("Results for Exam 2: ");
-        exam2 = scanner.nextInt();
-        
-        if(exam1 >= 50) {
-            System.out.println("You have passed exam 1");
-        } else {
-            System.out.println("You have FAILED exam 1");
-        }
-
-        if(exam2 >= 50) {
-            System.out.println("You have passed exam 2");
-        } else if(exam2 < 0) {
-            System.out.println("How did you get negative points?!");
-        } else {
-            System.out.println("You have FAILED exam 2");
-        }
-
-        if(exam1 >= 50 && exam2 >= 50) {
-            System.out.println("You have passed BOTH exams!");
+        switch(medalPlacement) {
+            case 1: System.out.println("GOLD MEDAL"); break;
+            case 2: System.out.println("SILVER MEDAL"); break;
+            case 3: System.out.println("BRONZE MEDAL"); break;
+            default: System.out.println("NO MEDAL"); break;
         }
     }
 }
