fed7e0a750eccf62c8e4cd0ee4fa76edb1867f7c
Kaupenjoe <N.Kaupenjohann@gmail.com>
2021-11-12 14:46:56 +0100

    methods

--- src/net/kaupenjoe/Main.java
+++ src/net/kaupenjoe/Main.java
@@ -1,45 +1,41 @@
 package net.kaupenjoe;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        /* LOOPS */
+        /* METHODS */
 
         String[] questions = new String[3];
         questions[0] = "How many States does the USA have?";
-        questions[1] = "What is the Capital of the United Kingdom";
-        questions[2] = "What is the chemical symbol for Iron";
+        questions[1] = "What is the Capital of the United Kingdom?";
+        questions[2] = "What is the chemical symbol for Iron?";
 
         String[] answers = new String[3];
         answers[0] = "50";
         answers[1] = "London";
         answers[2] = "Fe";
 
-        for(int i = 0; i < 100; i++) {
-            System.out.println(i);
-        }
+        outputQuestionAndAnswersXTimes(2, questions, answers);
 
-        for(int i = 0; i < questions.length; i++) {
+        System.out.println(sum(10, 12));
+
+    }
+
+    public static void outputQuestionAndAnswersXTimes(int amountOfOutputs, String[] questions, String[] answers) {
+        for (int y = 0; y < amountOfOutputs; y++) {
+            outputQuestionsAndAnswers(questions, answers);
+        }
+    }
+
+    public static void outputQuestionsAndAnswers(String[] questions, String[] answers) {
+        for (int i = 0; i < questions.length; i++) {
             System.out.println(questions[i]);
             System.out.println(answers[i]);
         }
-
-        // foreach
-        for(String question : questions) {
-            System.out.println(question);
-        }
-
-        Scanner scanner = new Scanner(System.in);
-
-        // while loop AND this is endless!
-        while(true) {
-            System.out.println("Do you want to continue?");
-            if(scanner.next().toLowerCase().equals("yes")) {
-                continue;
-            } else {
-                break;
-            }
-        }
     }
+
+    public static int sum(int x, int y) {
+        return x + y;
+    }
+
+
 }
