81fba3cd6388970bb1513dc63b817219e28c78d5
Kaupenjoe <N.Kaupenjohann@gmail.com>
2021-11-12 13:46:11 +0100

    arrays

--- src/net/kaupenjoe/Main.java
+++ src/net/kaupenjoe/Main.java
@@ -2,19 +2,37 @@ package net.kaupenjoe;
 
 public class Main {
     public static void main(String[] args) {
-        /* TERNARY OPERATOR */
+        /* ARRAYS */
 
-        int exam = 65;
-        int gift = 0;
+        String question1 = "How many States does the USA have?";
+        String question2 = "What is the Capital of the United Kingdom";
+        String question3 = "What is the chemical symbol for Iron";
 
-        if(exam >= 50) {
-            gift = 10;
-        } else {
-            gift = 0;
-        }
-        System.out.println(gift);
+        String answer1 = "50";
+        String answer2 = "London";
+        String answer3 = "Fe";
 
-        gift = (exam >= 50) ? 10 : 0;
-        System.out.println(gift);
+        String[] questions = new String[3];
+        questions[0] = "How many States does the USA have?";
+        questions[1] = "What is the Capital of the United Kingdom";
+        questions[2] = "What is the chemical symbol for Iron";
+
+        String[] answers = new String[3];
+        answers[0] = "50";
+        answers[1] = "London";
+        answers[2] = "Fe";
+
+        System.out.println(questions[0]);
+        System.out.println(answers[0]);
+
+        questions[0] = "How old is Ewan McGregor (in 2021)?";
+        System.out.println(questions[0]);
+        System.out.println(answers[0]);
+
+        System.out.println(questions.length);
+
+        // ArrayIndexOutOfBoundsException (Uncomment line below)
+        // System.out.println(questions[3]);
+
     }
 }
