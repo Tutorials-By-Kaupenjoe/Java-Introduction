6c2ce14a8dd9392a645495f145d17ce1fc14ee2e
Kaupenjoe <N.Kaupenjohann@gmail.com>
2021-11-13 08:07:44 +0100

    Collections

--- src/net/kaupenjoe/Main.java
+++ src/net/kaupenjoe/Main.java
@@ -1,116 +1,72 @@
 package net.kaupenjoe;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        /* EXERCISE & SOLUTION 2  */
+        /* ARRAYLIST, MAPS AND SETS (Collections) */
 
-        /*
-         * Exercise 2:
-         * a) Re-create the Trivia Game from before. This time with 5 Questions & 5 Answers!
-         *  Use Arrays to save the question and answers.
-         *  Create at least two methods that make the code more readable and easier to follow.
-         *    --> Hint: Duplicate code can usually be made into methods
-         *  BONUS:  Use an endless while loop with if statements so the user can play until they don't want anymore.
-         *
-         * OPTIONAL HARDER EXERCISE (Without Solution):
-         * b) Create a very simple version of Tik Tak Toe
-         *  Hints: Use one array to save the board. Think about what type of data you need!
-         *         Use methods to properly print out each row of the board.
-         *         Make it a two Player Game (making an AI is too advanced :P)
-         *         You will need to use Loops here as well!
-         *
-         */
-
-        String[] questions = new String[5];
+        String[] questions = new String[2];
         questions[0] = "How many States does the USA have?";
         questions[1] = "What is the Capital of the United Kingdom?";
-        questions[2] = "What is the chemical symbol for Iron?";
-        questions[3] = "How High is Mount Everest? (Answer in km)";
-        questions[4] = "What is the Answer to Life, the Universe and Everything?";
 
-        String[] answers = new String[5];
-        answers[0] = "50";
-        answers[1] = "London";
-        answers[2] = "Fe";
-        answers[3] = "8848";
-        answers[4] = "42";
+        List<String> questionsList = new ArrayList<>(); // No need to define the length
+        questionsList.add("What Language is spoke in Germany?");
+        questionsList.add("What is the Capital of Canada?");
+        questionsList.add("In what Hemisphere is Brazil?");
 
-        Scanner scanner = new Scanner(System.in);
-        int points = 0;
+        // Lists --> you can add entries dynamically
 
-        while (true) {
-            printTitle();
+        // Removing is fairly easy
+        questionsList.remove(1);
 
-            for(int i = 0; i < questions.length; i++) {
-                printQuestion(questions[i]);
-                String userAnswer = scanner.next();
+        System.out.println(questionsList.size());
 
-                if(isCorrectAnswer(userAnswer, answers[i])) {
-                    printYouHadCorrect();
-                    points = increasePointTotal(points);
-                    printCurrentPoints(points);
-                } else {
-                    printYouHadIncorrect();
-                    printTheCorrectAnswer(answers[i]);
-                }
-            }
+        System.out.println(questionsList.get(0));
 
-            printPointTotal(points);
-            printPlayAgain();
-            if(scanner.next().equals("Y")) {
-                points = 0;
-                continue;
-            } else {
-                break;
-            }
-        }
+        // List<int> numbers = new ArrayList<int>();
+        // Generics don't work with primitive datatypes
+        // Wrapper Classes you can use instead
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(42);
+        numbers.add(1337);
 
-        printGoodbye();
-    }
+        // MAPS
+        // map a Key to a certain Value. Called Key-Value Pairs
+        Map<String, String> CountryToCapital = new HashMap<>();
+        CountryToCapital.put("Germany", "Berlin");
+        CountryToCapital.put("France", "Paris");
+        CountryToCapital.put("Italy", "Rome");
+        CountryToCapital.put("USA", "Washington DC");
 
-    public static void printTitle() {
-        System.out.println("KAUPEN-TRIVA");
-    }
+        // To get a Value of a Map you have to supply it a Key
+        System.out.println(CountryToCapital.get("Germany"));
 
-    public static int increasePointTotal(int points) {
-        return points + 1;
-    }
+        System.out.println("Contains Key 'Germany'? " + CountryToCapital.containsKey("Germany"));
+        System.out.println("Contains Value 'London'? " + CountryToCapital.containsValue("London"));
 
-    public static void printCurrentPoints(int points) {
-        System.out.println("You currently have " + points + " Points!");
-    }
+        System.out.println(CountryToCapital.remove("France"));
 
-    public static void printPlayAgain() {
-        System.out.println("Do you wanna play again? Y / N");
-    }
+        // SETS
+        // A Set is a collection that contains not duplicates
+        Set<String> usernames = new HashSet<>();
+        usernames.add("Kaupenjoe");
+        usernames.add("Nanoattack");
 
-    public static void printPointTotal(int points) {
-        System.out.println("You had " + points + " Points!");
-    }
+        System.out.println(usernames.add("FRV"));
+        System.out.println(usernames.add("FRV"));
 
-    public static void printGoodbye() {
-        System.out.println("Thank you very much for playing KAUPEN-TRIVIA");
-    }
 
-    public static void printQuestion(String question) {
-        System.out.println(question);
-    }
+        /* ERROR YOU MIGHT RUN INTO */
+        // Uncomment to see Errors
 
-    public static boolean isCorrectAnswer(String userAnswer, String realAnswer) {
-        return userAnswer.equals(realAnswer);
-    }
+        // IndexOutOfBoundException
+        // If you try and pass in an Index that does not exist for a list
+        // System.out.println(questionsList.get(2));
 
-    public static void printYouHadCorrect() {
-        System.out.println("That was the correct Answer");
-    }
-
-    public static void printYouHadIncorrect() {
-        System.out.println("That was the incorrect Answer!");
-    }
-
-    public static void printTheCorrectAnswer(String realAnswer) {
-        System.out.println("The Correct Answer was: " + realAnswer);
+        // Not an Exception, BUT "null"
+        // If you pass in a Key into a Map that does not exist
+        // Your return value is going to be null (might lead to other errors down the line!)
+        // System.out.println(CountryToCapital.get("Malta"));
     }
 }
