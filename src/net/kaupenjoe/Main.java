558a04b85e32714cb2cfbf845bdfde93e21e631a
Kaupenjoe <N.Kaupenjohann@gmail.com>
2021-11-20 14:33:33 +0100

    generics

--- src/net/kaupenjoe/Main.java
+++ src/net/kaupenjoe/Main.java
@@ -1,49 +1,14 @@
 package net.kaupenjoe;
 
-import java.util.Scanner;
-import java.util.concurrent.Callable;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Supplier;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        /* LAMBDA EXPRESSIONS */
+        /* GENERICS */
+        List<Integer> numbers = new ArrayList<>();
 
-        // Functions as Variables
-        // Supplier, Consumer, Callable, Runnable, Function
+        Pair<Integer, ?> pair = new Pair<>(20, 2f);
 
-        // Supplier: no parameter, but return value
-        // () -> x
-        Supplier<Integer> supplier = () -> { return 10; };
-        System.out.println(supplier.get());
-
-        // Consumer: one paramtere, but not return value
-        // x -> ()
-        Consumer<String> consumer = (s) -> { System.out.println(s); };
-        consumer.accept("Hello World");
-
-        // Callable: no parameter, and return value
-        // () -> x throws Exception
-        Callable<Float> callable = () -> { return 100f / 0f; };
-
-        try
-        {
-            callable.call();
-        }
-        catch (Exception e)
-        {
-            System.out.println(e);
-        }
-
-        // Runnable: no paramters, no return value
-        // () -> ()
-        Runnable runnable = () -> { System.out.println("Hello World again"); };
-        runnable.run();
-
-        // Function: has parameter, and return value
-        // x -> y
-        Function<Double, Double> function = (r) -> { return r * 3.14159265; };
-        System.out.println(function.apply(10.0));
     }
 }
---
+++ src/net/kaupenjoe/Pair.java
@@ -0,0 +1,27 @@
+package net.kaupenjoe;
+
+public class Pair<X, Y> {
+    private X first;
+    private Y second;
+
+    public Pair(X first, Y second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public X getFirst() {
+        return first;
+    }
+
+    public void setFirst(X first) {
+        this.first = first;
+    }
+
+    public Y getSecond() {
+        return second;
+    }
+
+    public void setSecond(Y second) {
+        this.second = second;
+    }
+}
