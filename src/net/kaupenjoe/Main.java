33a73333ce99457966e707e9e879c547e7472b26
Kaupenjoe <N.Kaupenjohann@gmail.com>
2021-12-29 12:04:22 +0100

    Update Main.java

--- src/net/kaupenjoe/Main.java
+++ src/net/kaupenjoe/Main.java
@@ -1,49 +1,27 @@
 package net.kaupenjoe;
 
-import java.util.Scanner;
-import java.util.concurrent.Callable;
-import java.util.function.Consumer;
-import java.util.function.Function;
+import java.time.LocalDateTime;
 import java.util.function.Supplier;
 
 public class Main {
     public static void main(String[] args) {
-        /* LAMBDA EXPRESSIONS */
+        // parameter -> expression
 
-        // Functions as Variables
-        // Supplier, Consumer, Callable, Runnable, Function
+        LocalDateTime dateTime = LocalDateTime.now();
+        Supplier<LocalDateTime> dateSupplier = () -> LocalDateTime.now();
 
-        // Supplier: no parameter, but return value
-        // () -> x
-        Supplier<Integer> supplier = () -> { return 10; };
-        System.out.println(supplier.get());
+        System.out.println(dateTime);
+        System.out.println(dateSupplier.get());
 
-        // Consumer: one paramtere, but not return value
-        // x -> ()
-        Consumer<String> consumer = (s) -> { System.out.println(s); };
-        consumer.accept("Hello World");
-
-        // Callable: no parameter, and return value
-        // () -> x throws Exception
-        Callable<Float> callable = () -> { return 100f / 0f; };
-
-        try
-        {
-            callable.call();
-        }
-        catch (Exception e)
-        {
-            System.out.println(e);
+        System.out.println("Waiting for 3 Seconds!");
+        // Sleep for 3 seconds
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
 
-        // Runnable: no paramters, no return value
-        // () -> ()
-        Runnable runnable = () -> { System.out.println("Hello World again"); };
-        runnable.run();
-
-        // Function: has parameter, and return value
-        // x -> y
-        Function<Double, Double> function = (r) -> { return r * 3.14159265; };
-        System.out.println(function.apply(10.0));
+        System.out.println(dateTime);
+        System.out.println(dateSupplier.get());
     }
 }
